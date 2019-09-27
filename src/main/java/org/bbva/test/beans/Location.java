1195652478ae3c2d8ff35b9f051b4a7c8bd9b7f8
yolose100 <elvis.tellez.1234@gmail.com>
2019-09-27 11:11:22 -0500

    commit apicurio

---
+++ src/main/java/org/bbva/test/beans/Location.java
@@ -0,0 +1,95 @@
+
+package org.bbva.test.beans;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+
+/**
+ * Root Type for location
+ * <p>
+ * {
+ *     "id": 1,
+ *     "name": "International Inc Corporate Office",
+ *     "location": {
+ *         "lat": 51.5013673,
+ *         "lng": -0.1440787
+ *     },
+ *     "type": "headquarter",
+ *     "status": "1"
+ * }
+ * 
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "id",
+    "name",
+    "location",
+    "type",
+    "status"
+})
+public class Location {
+
+    @JsonProperty("id")
+    private Integer id;
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("location")
+    private Location__1 location;
+    @JsonProperty("type")
+    private String type;
+    @JsonProperty("status")
+    private String status;
+
+    @JsonProperty("id")
+    public Integer getId() {
+        return id;
+    }
+
+    @JsonProperty("id")
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonProperty("location")
+    public Location__1 getLocation() {
+        return location;
+    }
+
+    @JsonProperty("location")
+    public void setLocation(Location__1 location) {
+        this.location = location;
+    }
+
+    @JsonProperty("type")
+    public String getType() {
+        return type;
+    }
+
+    @JsonProperty("type")
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @JsonProperty("status")
+    public String getStatus() {
+        return status;
+    }
+
+    @JsonProperty("status")
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
